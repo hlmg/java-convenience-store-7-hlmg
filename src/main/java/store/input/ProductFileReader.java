e7511999f7f99a6b0d6d39820ac1b026f6e97436
hlmg <devhlmg@gmail.com>
2024-11-09 04:17:14 +0900

    refactor: 상품과 프로모션 연관관계 설정

--- src/main/java/store/input/ProductFileReader.java
+++ src/main/java/store/input/ProductFileReader.java
@@ -7,18 +7,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import store.model.Product;
+import store.model.Promotion;
 
 public class ProductFileReader {
 
     private static final String PATH = "src/main/resources/products.md";
     private static final String DELIMITER = ",";
+    private static final int INDEX_LINE = 1;
+    private static final int NAME = 0;
+    private static final int PRICE = 1;
+    private static final int QUANTITY = 2;
+    private static final int PROMOTION = 3;
 
-    public List<Product> getProducts() {
+    public List<Product> getProducts(List<Promotion> promotions) {
         Path path = Path.of(PATH);
         try (Stream<String> lines = Files.lines(path)) {
-            return lines.skip(1)
+            return lines.skip(INDEX_LINE)
                     .map(this::toStringList)
-                    .map(this::toProduct)
+                    .map(productInfo -> toProductData(productInfo, promotions))
                     .toList();
         } catch (IOException e) {
             throw new RuntimeException("상품 정보를 가져올 수 없습니다.");
@@ -30,13 +36,20 @@ public class ProductFileReader {
                 .toList();
     }
 
-    private Product toProduct(List<String> productInfo) {
+    private Product toProductData(List<String> productInfo, List<Promotion> promotions) {
+        Promotion promotion = findPromotion(promotions, productInfo.get(PROMOTION));
         return new Product(
-                productInfo.get(0),
-                toInt(productInfo.get(1)),
-                toInt(productInfo.get(2)),
-                productInfo.get(3)
-        );
+                productInfo.get(NAME),
+                toInt(productInfo.get(PRICE)),
+                toInt(productInfo.get(QUANTITY)),
+                promotion);
+    }
+
+    private Promotion findPromotion(List<Promotion> promotions, String promotionName) {
+        return promotions.stream()
+                .filter(promotion -> promotion.isSameName(promotionName))
+                .findAny()
+                .orElse(null);
     }
 
     private int toInt(String value) {
--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -5,9 +5,9 @@ public class Product {
     private final String name;
     private final int price;
     private final int quantity;
-    private final String promotion;
+    private final Promotion promotion;
 
-    public Product(String name, int price, int quantity, String promotion) {
+    public Product(String name, int price, int quantity, Promotion promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
