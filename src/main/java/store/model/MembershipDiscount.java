453f9118bc7ba3da634f691601928ab2340092ee
hlmg <devhlmg@gmail.com>
2024-11-10 15:12:31 +0900

    feat: 멤버십 할인 금액을 계산하는 기능 구현

---
+++ src/main/java/store/model/MembershipDiscount.java
@@ -0,0 +1,19 @@
+package store.model;
+
+public class MembershipDiscount {
+
+    private static final int DISCOUNT_LIMIT = 8000;
+    private static final double DISCOUNT_RATE = 0.3;
+    private static final double ROUNDING_UNIT = 10.0;
+
+    public int getDiscountAmount(int price) {
+        double discountPrice = price * DISCOUNT_RATE;
+        discountPrice = roundToUnit(discountPrice);
+        return (int) Math.min(DISCOUNT_LIMIT, discountPrice);
+    }
+
+    private double roundToUnit(double discountPrice) {
+        return Math.round(discountPrice / ROUNDING_UNIT) * ROUNDING_UNIT;
+    }
+
+}
---
+++ src/test/java/store/model/MembershipDiscountTest.java
@@ -0,0 +1,29 @@
+package store.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+class MembershipDiscountTest {
+
+    @CsvSource(textBlock = """
+            10000,3000
+            26640,7990
+            26650,8000
+            30000,8000
+            """)
+    @ParameterizedTest
+    void 할인액을_계산할_수_있다(int price, int expected) {
+        // given
+        MembershipDiscount membershipDiscount = new MembershipDiscount();
+
+        // when
+        int discountAmount = membershipDiscount.getDiscountAmount(price);
+
+        // then
+        assertThat(discountAmount).isEqualTo(expected);
+    }
+
+}
